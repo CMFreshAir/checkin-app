164dd9692e38b2c73457614a62a27ff84c0f2fd7
CMFreshAir <cmfreshair@gmail.com>
2026-05-09 23:31:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // ============================================================
 
 const CONFIG = {
-  API_URL:      'https://script.google.com/macros/s/AKfycbyvaq8Tih6xvUmGWZ_IUG1SAymkP1u2fSm_CsrSHNAIPkFxWetnzwJN7KW_MK7YnsXCeA/exec',
+  API_URL:      'https://script.google.com/macros/s/AKfycbyvaq8Tih6xvUmGWZ_.../exec',
   APP_NAME:     'Employee Check-In System',
   COMPANY_NAME: 'Flexmart Inter'  // ← เปลี่ยนชื่อบริษัท
 };
